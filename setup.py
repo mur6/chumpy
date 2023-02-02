e4821866bf6a642e0e662f36d48113de0f8edb19
MURAKI Taichi <muraki.taichi@gmail.com>
2023-02-02 16:56:11 +0900

    Fix setup config.

--- setup.py
+++ setup.py
@@ -5,32 +5,16 @@ See LICENCE.txt for licensing and contact information.
 """
 
 from distutils.core import setup
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
-from runpy import run_path
 
-install_reqs = parse_requirements('requirements.txt', session=False)
-try:  # for pip < 20.1
-    install_requires = [str(ir.req) for ir in install_reqs]
-except AttributeError:  # for pip >= 20.1
-    install_requires = [str(ir.requirement) for ir in install_reqs]
-
-def get_version():
-    namespace = run_path('chumpy/version.py')
-    return namespace['version']
 
 setup(name='chumpy',
-    version=get_version(),
+    version="0.71",
     packages = ['chumpy'],
     author='Matthew Loper',
     author_email='matt.loper@gmail.com',
     url='https://github.com/mattloper/chumpy',
     description='chumpy',
     license='MIT',
-    install_requires=install_requires,
-
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         # How mature is this project? Common values are
